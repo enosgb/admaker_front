4ee850c50f24df2bb7404e8eeb8c7148d663cb7d
Enos Gabriel <enosgb55@gmail.com>
2025-12-21 22:19:59 -0300

    feat(components): LogoLoading, MiniAlert

---
+++ src/components/alerts/MiniAlert.vue
@@ -0,0 +1,17 @@
+<template>
+  <transition name="fade">
+    <div v-if="eventName" :class="[
+      bgColor,
+      'text-white px-4 py-2 rounded shadow-md z-10 absolute top-2 left-1/2 -translate-x-1/2'
+    ]">
+      {{ eventName }}
+    </div>
+  </transition>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  eventName: string | null
+  bgColor?: string
+}>()
+</script>
---
+++ src/components/loading/LogoLoading.vue
@@ -0,0 +1,14 @@
+<template>
+  <div v-if="loading" class="flex justify-center m-2">
+    <img class="w-10 animate-spin" src="@/assets/logos/logo_admaker.png" alt="Logo Ad Maker">
+  </div>
+</template>
+
+<script setup lang="ts">
+
+
+defineProps<{
+  loading: boolean;
+}>();
+
+</script>
